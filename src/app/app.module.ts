299dd7ef3329a1115005c559d333d86c9c639fb2
Naioky <wfiaux2@gmail.com>
2022-08-15 04:15:33 -0300

    first commit
    #00 init and Add footer/header/nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { FiltersComponent } from './components/shared/bookstore-app/filters/filters.component';
+import { ProductListComponent } from './components/shared/bookstore-app/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    FiltersComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
